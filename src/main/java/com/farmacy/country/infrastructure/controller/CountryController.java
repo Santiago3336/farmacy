5c2d0a281d3debcc9734fc3caf10ba1817a3fe93
Santiago <j.santiagolseon@gmail.com>
2024-08-15 08:09:25 -0500

    fix: :bug:

--- src/main/java/com/farmacy/country/infrastructure/controller/CountryController.java
+++ src/main/java/com/farmacy/country/infrastructure/controller/CountryController.java
@@ -1,8 +1,23 @@
 package com.farmacy.country.infrastructure.controller;
 
+import com.farmacy.country.domain.entity.Country;
 import com.farmacy.country.domain.service.CountryService;
 
 public class CountryController {
-    private CountryService countryService;
-    
+    private final CountryService countryService;
+
+    // Constructor donde se inicializa countryService
+    public CountryController(CountryService countryService) {
+        this.countryService = countryService;
+    }
+
+    // Método que usa countryService
+    public void createCountry(Country country) {
+        countryService.createCountry(country);
+    }
+
+    // Otros métodos que pueden usar countryService
+    public Country getCountry(String code) {
+        return countryService.readCountry(code).orElse(null);
+    }
 }
