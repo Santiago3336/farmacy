41cd8b6e43354d33651eb9ee60a0e0657b54efd1
Santiago <j.santiagolseon@gmail.com>
2024-08-15 22:39:22 -0500

    fix: :card_file_box: fix db

--- target/classes/database/ddl.sql
+++ target/classes/database/ddl.sql
@@ -1,47 +1,47 @@
+DROP DATABASE IF EXISTS farmacy;
+
+CREATE DATABASE farmacy;
+
+USE farmacy_db;
+
 CREATE TABLE country (
-    codecountry VARCHAR(5),
-    namecountry VARCHAR(50),
-    CONSTRAINT pk_codecountry PRIMARY KEY (codecountry)
+    codecountry VARCHAR(5) PRIMARY KEY,
+    namecountry VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE region (
-    codereg VARCHAR(5),
-    namereg VARCHAR(50),
-    codecountry VARCHAR(5),
-    CONSTRAINT pk_codereg PRIMARY KEY (codereg),
-    CONSTRAINT fk_codecountry_country FOREIGN KEY (codecountry) REFERENCES country(codecountry)
+    codereg VARCHAR(5) PRIMARY KEY,
+    namereg VARCHAR(50) NOT NULL,
+    codecountry VARCHAR(5) NOT NULL,
+    CONSTRAINT fk_country FOREIGN KEY (codecountry) REFERENCES country(codecountry)
 );
 
 CREATE TABLE laboratory (
-    idlab SERIAL,
-    namelab VARCHAR(50),
-    codereg VARCHAR(5),
-    CONSTRAINT pk_idlab PRIMARY KEY (idlab),
-    CONSTRAINT fk_codereg_region FOREIGN KEY (codereg) REFERENCES region(codereg)
+    idlab INT AUTO_INCREMENT PRIMARY KEY,
+    namelab VARCHAR(50) NOT NULL,
+    codereg VARCHAR(5) NOT NULL,
+    CONSTRAINT fk_region FOREIGN KEY (codereg) REFERENCES region(codereg)
 );
 
 CREATE TABLE activeprinciple (
-    idap SERIAL,
-    nameap VARCHAR(50),
-    CONSTRAINT pk_idap PRIMARY KEY (idap)
+    idap INT AUTO_INCREMENT PRIMARY KEY,
+    nameap VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE unitmeasurement (
-    idum SERIAL,
-    nameum VARCHAR(50),
-    CONSTRAINT pk_idum PRIMARY KEY (idum)
+    idum INT AUTO_INCREMENT PRIMARY KEY,
+    nameum VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE modeadministration (
-    id SERIAL PRIMARY KEY,
-    descriptionmode VARCHAR(60),
-    CONSTRAINT pk_id PRIMARY KEY (id)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    descriptionmode VARCHAR(60) NOT NULL
 );
 
 CREATE TABLE medicine (
-    id SERIAL PRIMARY KEY,
-    proceedings VARCHAR(100),
-    namemedic VARCHAR(100),
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    proceedings VARCHAR(100) NOT NULL,
+    namemedic VARCHAR(100) NOT NULL,
     healthregister VARCHAR(20),
     description VARCHAR(50),
     codecomposition VARCHAR(10),
@@ -49,48 +49,46 @@ CREATE TABLE medicine (
     idlab INT,
     codeum INT,
     namerol VARCHAR(50),
-    CONSTRAINT pk_id PRIMARY KEY (id),
-    CONSTRAINT fk_idap_activeprinciple FOREIGN KEY (idap) REFERENCES activeprinciple(idap),
-    CONSTRAINT fk_idlab_laboratory FOREIGN KEY (idlab) REFERENCES laboratory(idlab),
-    CONSTRAINT fk_codeum_unitmeasurement FOREIGN KEY (codeum) REFERENCES unitmeasurement(idum)
+    CONSTRAINT fk_activeprinciple FOREIGN KEY (idap) REFERENCES activeprinciple(idap),
+    CONSTRAINT fk_laboratory FOREIGN KEY (idlab) REFERENCES laboratory(idlab),
+    CONSTRAINT fk_unitmeasurement FOREIGN KEY (codeum) REFERENCES unitmeasurement(idum)
+);
+
+CREATE TABLE city (
+    codecity VARCHAR(5) PRIMARY KEY,
+    namecity VARCHAR(50) NOT NULL,
+    codereg VARCHAR(5),
+    CONSTRAINT fk_region_city FOREIGN KEY (codereg) REFERENCES region(codereg)
 );
 
 CREATE TABLE customer (
-    idcustomer VARCHAR(20),
-    namecustomer VARCHAR(50),
-    lastnamecustomer VARCHAR(50),
+    idcustomer VARCHAR(20) PRIMARY KEY,
+    namecustomer VARCHAR(50) NOT NULL,
+    lastnamecustomer VARCHAR(50) NOT NULL,
     codecitycustomer VARCHAR(5),
     emailcustomer VARCHAR(100),
     brrdate DATE,
-    latitude FLOAT(8),
-    longitude FLOAT(8),
-    CONSTRAINT pk_idcustomer PRIMARY KEY (idcustomer)
+    latitud FLOAT,
+    longitud FLOAT,
+    CONSTRAINT fk_city_customer FOREIGN KEY (codecitycustomer) REFERENCES city(codecity)
 );
 
 CREATE TABLE pharmacy (
-    idfarmacy SERIAL,
-    namefarmacy VARCHAR(60),
+    idfarmacy INT AUTO_INCREMENT PRIMARY KEY,
+    namefarmacy VARCHAR(60) NOT NULL,
     addressfarmacy VARCHAR(50),
-    latfarmacy FLOAT(8),
-    longfarmacy FLOAT(8),
+    latfarmacy FLOAT,
+    longfarmacy FLOAT,
     codecityfarm VARCHAR(5),
     logo_farmacy VARCHAR(50),
-    CONSTRAINT pk_idfarmacy PRIMARY KEY (idfarmacy)
+    CONSTRAINT fk_city_pharmacy FOREIGN KEY (codecityfarm) REFERENCES city(codecity)
 );
 
 CREATE TABLE pharmacymedicine (
     idfarmacy INT,
     idmedicinefarm INT,
-    price FLOAT(8),
-    CONSTRAINT pk_idfarmacy_idmedicinefarm PRIMARY KEY (idfarmacy, idmedicinefarm),
-    CONSTRAINT fk_idfarmacy_pharmacy FOREIGN KEY (idfarmacy) REFERENCES pharmacy(idfarmacy),
-    CONSTRAINT fk_idmedicinefarm_medicine FOREIGN KEY (idmedicinefarm) REFERENCES medicine(id)
-);
-
-CREATE TABLE city (
-    codecity VARCHAR(5),
-    namecity VARCHAR(50),
-    codereg VARCHAR(5),
-    CONSTRAINT pk_codecity PRIMARY KEY (codecity),
-    CONSTRAINT fk_codereg_region FOREIGN KEY (codereg) REFERENCES public.region(codereg)
+    price FLOAT NOT NULL,
+    PRIMARY KEY (idfarmacy, idmedicinefarm),
+    CONSTRAINT fk_pharmacy FOREIGN KEY (idfarmacy) REFERENCES pharmacy(idfarmacy),
+    CONSTRAINT fk_medicine FOREIGN KEY (idmedicinefarm) REFERENCES medicine(id)
 );
